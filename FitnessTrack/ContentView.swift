f2d77e9f91f4e46430bb04bba20e856ccade9a97
Yufei <tao.yufei@icloud.com>
2021-05-26 12:02:45 -0700

    Initial Commit

---
+++ FitnessTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessTrack
+//
+//  Created by Yufei on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessTrack/FitnessTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackApp.swift
+//  FitnessTrack
+//
+//  Created by Yufei on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
